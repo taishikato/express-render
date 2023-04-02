1447348d263bdcb5752dbfbe3fa5abaca98a62ac
taishikato <taishi.k0903@gmail.com>
2023-04-02 13:39:06 -0700

    Try background job

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { createClient } = require("@supabase/supabase-js");
 const express = require("express");
 const bodyParser = require("body-parser");
 const asyncHandler = require("express-async-handler");
-
+const Queue = require("bull");
 const app = express();
 
 const port = process.env.PORT || 3001;
@@ -16,6 +16,71 @@ const supabaseAdmin = createClient(
 );
 
 const jsonParser = bodyParser.json();
+
+const vectorQueue = new Queue(
+  "vector data transcoding",
+  "redis://red-cgkub864dad69r4uhiug:6379"
+);
+
+vectorQueue.process(async (job, done) => {
+  const jobData = job.data;
+  const { filterId, sentences } = jobData;
+
+  await pinecone.init({
+    environment: "us-east1-gcp",
+    apiKey: process.env.PINECONE_KEY,
+  });
+
+  const index = pinecone.Index("test-index");
+
+  const textArray = sentences.map((t) => t.text);
+
+  let i = 0;
+  const vectorPayload = [];
+  console.log("Start: Calling Open AI embeddings API...");
+  for (const sentence of textArray) {
+    console.log(`Number: ${i}`);
+    const res = await fetch("https://api.openai.com/v1/embeddings", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        input: sentence,
+        model: "text-embedding-ada-002",
+      }),
+    });
+
+    const json = await res.json();
+
+    vectorPayload.push({
+      id: `${filterId}-${i}`,
+      values: json.data[0].embedding,
+      metadata: {
+        text: sentence,
+        filterId,
+      },
+    });
+
+    i++;
+  }
+
+  console.log("Upsert to Pinecone...");
+  const upsertResponse = await index.upsert({
+    upsertRequest: {
+      vectors: vectorPayload,
+      namespace: "example-namespace",
+    },
+  });
+
+  await supabaseAdmin
+    .from("documents")
+    .update({ is_generating_vector_data: false })
+    .eq("filter_id", filterId);
+
+  done();
+});
 
 app.get("/", (req, res, next) => {
   console.log("Info: / is called!");
@@ -33,60 +98,68 @@ app.post(
 
     console.log("filterId", filterId);
 
-    await pinecone.init({
-      environment: "us-east1-gcp",
-      apiKey: process.env.PINECONE_KEY,
-    });
+    const job = {
+      sentences,
+      filterId,
+    };
 
-    const index = pinecone.Index("test-index");
+    console.log("Info: Job set");
+    await routineJobsQueue.add(job);
 
-    const textArray = sentences.map((t) => t.text);
+    // await pinecone.init({
+    //   environment: "us-east1-gcp",
+    //   apiKey: process.env.PINECONE_KEY,
+    // });
 
-    let i = 0;
-    const vectorPayload = [];
-    console.log("Start: Calling Open AI embeddings API...");
-    for (const sentence of textArray) {
-      console.log(`Number: ${i}`);
-      const res = await fetch("https://api.openai.com/v1/embeddings", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          input: sentence,
-          model: "text-embedding-ada-002",
-        }),
-      });
+    // const index = pinecone.Index("test-index");
 
-      const json = await res.json();
+    // const textArray = sentences.map((t) => t.text);
 
-      vectorPayload.push({
-        id: `${filterId}-${i}`,
-        values: json.data[0].embedding,
-        metadata: {
-          text: sentence,
-          filterId,
-        },
-      });
+    // let i = 0;
+    // const vectorPayload = [];
+    // console.log("Start: Calling Open AI embeddings API...");
+    // for (const sentence of textArray) {
+    //   console.log(`Number: ${i}`);
+    //   const res = await fetch("https://api.openai.com/v1/embeddings", {
+    //     method: "POST",
+    //     headers: {
+    //       Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       input: sentence,
+    //       model: "text-embedding-ada-002",
+    //     }),
+    //   });
 
-      i++;
-    }
+    //   const json = await res.json();
 
-    console.log("Upsert to Pinecone...");
-    const upsertResponse = await index.upsert({
-      upsertRequest: {
-        vectors: vectorPayload,
-        namespace: "example-namespace",
-      },
-    });
+    //   vectorPayload.push({
+    //     id: `${filterId}-${i}`,
+    //     values: json.data[0].embedding,
+    //     metadata: {
+    //       text: sentence,
+    //       filterId,
+    //     },
+    //   });
 
-    console.log("upsertResponse", upsertResponse);
+    //   i++;
+    // }
 
-    await supabaseAdmin
-      .from("documents")
-      .update({ is_generating_vector_data: false })
-      .eq("filter_id", filterId);
+    // console.log("Upsert to Pinecone...");
+    // const upsertResponse = await index.upsert({
+    //   upsertRequest: {
+    //     vectors: vectorPayload,
+    //     namespace: "example-namespace",
+    //   },
+    // });
+
+    // console.log("upsertResponse", upsertResponse);
+
+    // await supabaseAdmin
+    //   .from("documents")
+    //   .update({ is_generating_vector_data: false })
+    //   .eq("filter_id", filterId);
 
     console.log("End: Request successfully done.");
     return res.json({
